c077d6eea7ded8028979217d06e803b9296015d0
João Vinicius Pereira Marinho <joaoviinicius9@gmail.com>
2024-03-09 23:04:00 -0300

    biblioteca

---
+++ todos/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Todo
+
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
